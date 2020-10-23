9d641e53cb5d3fe351326ff4d2457f308cef548b
PatrykDubinski <dubinskipatryk99@gmail.com>
2020-10-23 14:59:25 +0200

    Added categories menu with MongoDB connection

--- src/components/BodyMenu/BodyMenu.js
+++ src/components/BodyMenu/BodyMenu.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BodyMenu.css";
 
+import axios from "../../axios";
+
 const BodyMenu = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get("/categories");
+
+      setCategories(request.data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(categories);
+
   return (
-    <div className="bodyMenu">
-      <div className="bodyMenu__menuItem first">
-        <h1>Test</h1>
+    <>
+      <h1 className="menuTitle">What's on the menu?</h1>
+      <div className="bodyMenu">
+        {categories.map((el) => {
+          console.log(el);
+          return (
+            <div className="bodyMenu__menuItem" key={el.id}>
+              <img src={el.imgUrl} alt={el.name} />
+              <h2>{el.name}</h2>
+            </div>
+          );
+        })}
       </div>
-      <div className="bodyMenu__menuItem second">
-        <h1>Test</h1>
-      </div>
-      <div className="bodyMenu__menuItem third">
-        <h1>Test</h1>
-      </div>
-      <div className="bodyMenu__menuItem fourth">
-        <h1>Test</h1>
-      </div>
-      <div className="bodyMenu__menuItem fifth">
-        <h1>Test</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
