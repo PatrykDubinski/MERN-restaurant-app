cf029067e425ab03f2a432d81887391d50abf67c
PatrykDubinski <dubinskipatryk99@gmail.com>
2020-10-23 08:59:42 +0200

    Added grid menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import BodyMenu from "./components/BodyMenu/BodyMenu";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <BodyMenu />
     </div>
   );
 }
---
+++ src/components/BodyMenu/BodyMenu.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./BodyMenu.css";
+
+const BodyMenu = () => {
+  return (
+    <div className="bodyMenu">
+      <div className="bodyMenu__menuItem first">
+        <h1>Test</h1>
+      </div>
+      <div className="bodyMenu__menuItem second">
+        <h1>Test</h1>
+      </div>
+      <div className="bodyMenu__menuItem third">
+        <h1>Test</h1>
+      </div>
+      <div className="bodyMenu__menuItem fourth">
+        <h1>Test</h1>
+      </div>
+      <div className="bodyMenu__menuItem fifth">
+        <h1>Test</h1>
+      </div>
+    </div>
+  );
+};
+
+export default BodyMenu;
